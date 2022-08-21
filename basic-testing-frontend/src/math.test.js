d136b5c98b387422fae4bdb2e72f18504ee2451c
Michael Manlulu <michaeldmanlulu@gmail.com>
2022-08-21 20:36:17 +0800

    3.6. implement the AAA pattern

--- basic-testing-frontend/src/math.test.js
+++ basic-testing-frontend/src/math.test.js
@@ -2,6 +2,13 @@ import { expect, it } from 'vitest';
 import { add } from './math';
 
 it('should summarze all number values in an array', () => {
-  const result = add([1, 2, 3]);
-  expect(result).toBe(6);
+  // Arrange
+  const numbers = [1, 2, 3];
+
+  // Act
+  const result = add(numbers);
+
+  // Assert
+  const expectedResult = numbers.reduce((prev, cur) => prev + cur, 0);
+  expect(result).toBe(expectedResult);
 });
