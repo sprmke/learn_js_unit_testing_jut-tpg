adad43db347295049d5bb45acbe8060818d36a7c
Michael Manlulu <michaeldmanlulu@gmail.com>
2022-08-22 00:37:18 +0800

    3.11 throw error when provided with multiple arguments

--- basic-testing-frontend/src/math.test.js
+++ basic-testing-frontend/src/math.test.js
@@ -45,3 +45,15 @@ it('should throw an error if no value is passed into the function', () => {
   };
   expect(resultFn).toThrow();
 });
+
+it('should throw an error if provided with multiple arguments instead of an array', () => {
+  // Arrange
+  const num1 = 1;
+  const num2 = 2;
+  // Act
+  const resultFn = () => {
+    add(num1, num2);
+  };
+  // Assert
+  expect(resultFn).toThrow();
+});
